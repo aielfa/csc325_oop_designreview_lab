bd23fd9789695f83104d5370775160dd83d65663
Francis Aiello <aielfa@farmingdale.edu>
2024-11-01 14:33:24 -0400

    Human .toString

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Human.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Human.java
@@ -39,4 +39,9 @@ public abstract class Human {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	@Override
+	public String toString() {
+		return "Name: " + name + ", Age: " + age + ", Address: " + address;
+	}
 }
